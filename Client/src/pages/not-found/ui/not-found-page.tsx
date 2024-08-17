3b2f1c0926238903d1ee0dbb39f2fb6a921ab90f
Edanriell <Edanriell@gmail.com>
2024-08-17 11:52:03 +0300

    finished ui

---
+++ Client/src/pages/not-found/ui/not-found-page.tsx
@@ -0,0 +1,5 @@
+import "./not-found-page.less";
+
+export const NotFoundPage = () => {
+	return <h1>Page Not Found</h1>;
+};
--- Client/src/pages/routes.ts
+++ Client/src/pages/routes.ts
@@ -1,8 +1,13 @@
 import { MainPage } from "./main/ui";
+import { NotFoundPage } from "./not-found/ui";
 
 export const routes = [
 	{
 		path: "/",
 		component: MainPage
+	},
+	{
+		path: "*",
+		component: NotFoundPage
 	}
 ];
